748edf5b2daa3e9902306f21180b4cc6da5577ff
許家瑋 <shelling@cpan.org>
2010-12-01 17:36:37 +0000

    Import of SHELLING/App-LDAP-0.02 from CPAN.
    gitpan-cpan-distribution: App-LDAP
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHELLING/App-LDAP-0.02.tar.gz
    gitpan-cpan-author:       SHELLING
    gitpan-cpan-maturity:     released

--- lib/App/LDAP.pm
+++ lib/App/LDAP.pm
@@ -1,9 +1,23 @@
+use 5.010;
+use strict;
+use warnings;
+
 package App::LDAP;
 use strict;
 use warnings;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
+
+use Rubyish::Attribute;
+
+attr_accessor "config", "connection";
+
+use Net::LDAP;
+use Term::ReadPassword;
 
 use App::LDAP::Command;
+use App::LDAP::Config;
+use Net::LDAP::Extension::WhoAmI;
+
 
 sub new {
   my $class = shift;
@@ -12,8 +26,37 @@ sub new {
 
 sub run {
   my ($self,) = @_;
-  App::LDAP::Command->dispatch;
+  $self->config( App::LDAP::Config->read );
+  $self->connect;
+  App::LDAP::Command->dispatch(app_info => $self);
 }
+
+sub connect {
+  my ($self) = @_;
+  my $config = $self->config;
+  my $ldap = Net::LDAP->new(
+    $config->{uri},
+    port       => $config->{port},
+    version    => $config->{ldap_version},
+    onerror    => 'die',
+  );
+  if ($< == 0) {
+    my $userdn = $config->{rootdn};
+    my $userpw = $config->{rootpw};
+    $ldap->bind($userdn, password => $userpw);
+  } else {
+    my ($base, $scope) = split /\?/, $config->{nss_base_passwd};
+    my $userdn = $ldap->search( base => $base, scope => $scope, filter => "uidNumber=$<")
+                      ->entry(0)
+                      ->dn;
+    my $userpw = read_password("password: ");
+    $ldap->bind($userdn, password => $userpw);
+  }
+  say "bind as ", $ldap->who_am_i->response;
+  $self->connection($ldap);
+}
+
+
 
 
 1;
